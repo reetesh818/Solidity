bd5f0daade00acd1d5fc6459b3315f698f7c09ee
reetesh818 <75972514+reetesh818@users.noreply.github.com>
2021-09-22 22:10:47 +0530

    created a contract to find sum of two numbers

--- add.sol
+++ add.sol
@@ -1,1 +1,10 @@
 pragma solidity ^0.8.3;
+contract add{
+    uint8 public a=20;
+    uint8 public b=30;
+    uint8 public result;
+    
+    function findSum() public{
+        result = a+b;
+    }
+}
